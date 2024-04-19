1cb2d53746721786a7a6b9ae9b411d868863b172
Lozzan21 <barreralozan@gmail.com>
2024-04-18 20:42:49 -0600

    aqlo

--- app.js
+++ app.js
@@ -1,32 +1,23 @@
 const express = require('express');
 const ejs = require('ejs');
 const PDFDocument = require('pdfkit');
+const table = require('pdfkit-table');
 const fs = require('fs');
 const bodyParser = require('body-parser');
 const mysql = require('mysql');
-const path = require('path'); // Agrega esta línea
-
+const path = require('path'); 
+const pdfGeneratorRouter = require('./pdfGenerator');
+const imagePath = path.join(__dirname, 'img/Membrete.jpg');
 const app = express();
+const connection = require('./db'); 
+
+
+
+app.use('/generar', pdfGeneratorRouter);
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 const port = 3000;
 
-// Configurar conexión a la base de datos MySQL
-const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'cetis'
-});
-
-// Conectar a la base de datos
-connection.connect((err) => {
-    if (err) {
-        console.error('Error al conectar a la base de datos:', err);
-        return;
-    }
-    console.log('Conexión a la base de datos MySQL establecida.');
-});
 
 // Configurar body-parser
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -490,113 +481,155 @@ app.post('/login', (req, res) => {
 });
 
 
-
-
-
-
-
 // Generar PDF y guardar en la base de datos
 app.post('/generate-pdf', (req, res) => {
     const data = req.body;
 
     // Función para agregar la imagen en el pie de página
-function agregarImagenEnPieDePagina(pdf) {
-  const imagenPath = path.join(__dirname, 'img/Membrete-bj.jpg');
-  const imagenAncho = 550;
-  const imagenAlto = 100;
-  const margen = 20;
-
-  const paginas = pdf.bufferedPageRange().count;
-
-  for (let i = 0; i < paginas; i++) {
-      pdf.switchToPage(i);
-      pdf.image(imagenPath, margen, pdf.page.height - margen - imagenAlto, {
-          width: imagenAncho,
-          height: imagenAlto
-      });
-  }
-}
-
-    // Generar el PDF
-    const pdf = new PDFDocument();
-    let buffers = [];
-    pdf.on('data', buffers.push.bind(buffers));
-    pdf.on('end', () => {
-        const pdfData = Buffer.concat(buffers);
-
-        // Guardar el PDF en la base de datos
-        const sql = 'INSERT INTO pdf_data (asunto, maestro, especialidad, materia, grupo, fecha,texto, pdf) VALUES (?, ?, ?, ?, ?, ?, ?, ?)';
-        const values = [data.asunto, data.maestro, data.especialidad, data.materia, data.grupo, data.fecha, data.texto, pdfData];
-
-        connection.query(sql, values, (err, result) => {
-            if (err) {
-                console.error('Error al guardar el PDF en la base de datos:', err);
-                return res.status(500).send('Error interno del servidor');
-            }
-
-            console.log('PDF guardado en la base de datos.');
-            res.set({
-                'Content-Type': 'application/pdf',
-                'Content-Length': pdfData.length,
-                'Content-Disposition': 'attachment; filename="oficio.pdf"'
+    function agregarImagenEnPieDePagina(pdf) {
+        const imagenPath = path.join(__dirname, 'img/Membrete-bj.jpg');
+        const imagenAncho = 550;
+        const imagenAlto = 100;
+        const margen = 20;
+      
+        const paginas = pdf.bufferedPageRange().count;
+      
+        for (let i = 0; i < paginas; i++) { // Definir la variable i aquí
+            pdf.switchToPage(i);
+            pdf.image(imagenPath, margen, pdf.page.height - margen - imagenAlto, {
+                width: imagenAncho,
+                height: imagenAlto
             });
-            res.send(pdfData);
+        }
+      }
+
+
+    // Función para construir el contenido del PDF
+    function construirContenidoPDF(doc) {
+        doc.image(imagePath, {
+            fit: [350, 350], 
+            align: 'center',
         });
-    });
+        doc.moveDown(4);
 
-    
-    // Construir el contenido del PDF
-    const imagePath = path.join(__dirname, 'img/Membrete.jpg');
-    pdf.image(imagePath, {
-        fit: [350, 350], 
-        align: 'center',
-    });
-    pdf.moveDown(4);
+        doc.font('Helvetica-Bold').fontSize(12).text(`ÁREA: SERVICIOS DOCENTES \n OFICIO No. 220 (CE-148)${data.numero}/2024`, {
+            align: 'right'
+        });
+        doc.text(`ASUNTO: ${data.asunto}`,{
+            align: 'right'
+        });
+        doc.text(`Durango, Dgo. ${fechaEscrita(data.fecha)}`,{
+            align: 'right'
+        });
+        doc.moveDown(2);
+        doc.text(` ${data.maestro} \nDOCENTE DEL PLANTEL`);
+        doc.moveDown(4);
+        doc.text(`Texto: ${data.texto}`);
+        doc.moveDown(2);
 
-    pdf.font('Helvetica-Bold').fontSize(12).text('ÁREA: SERVICIOS DOCENTES \n OFICIO No. 220 (CE-148)946/2024', {
-      align: 'right'
-  });
-    pdf.text(`ASUNTO: ${data.asunto}`,{
-      align: 'right'
-  });
-  pdf.text(`Durango, Dgo. ${fechaEscrita(data.fecha)}`,{
-    align: 'right'
+        doc.moveDown(15);
+        doc.font('Helvetica-Bold').fontSize(12)
+            .text('ATENTAMENTE.', 70, 480)
+            .text('M.T.I. JOEL VÀZQUEZ RÌOS', 70, 580)
+            .text('JEFE DEL DEPARTAMENTO', 70, 595)    
+            .text('DE SERVICIOS DOCENTES', 70, 610); 
+
+        agregarImagenEnPieDePagina(doc);
+
+        doc.end();
+
+        function fechaEscrita(fecha) {
+            const meses = [
+                'enero', 'febrero', 'marzo', 'abril', 'mayo', 'junio',
+                'julio', 'agosto', 'septiembre', 'octubre', 'noviembre', 'diciembre'
+            ];
+            
+            const partesFecha = fecha.split('-');
+            const dia = partesFecha[2];
+            const mesNum = parseInt(partesFecha[1]) - 1;
+            const mes = meses[mesNum];
+            const año = partesFecha[0];
+            
+            return `${dia} de ${mes} de ${año}`;
+        }
+    }
+
+    if (req.query.doc) {
+        
+        // Generar el documento DOC
+        const doc = new Docxtemplater();
+        const content = fs.readFileSync(path.resolve(__dirname, 'plantilla.docx'), 'binary');
+        doc.loadZip(content);
+
+        // Reemplazar los marcadores de posición en la plantilla con los datos proporcionados
+        doc.setData({
+            asunto: data.asunto,
+            maestro: data.maestro,
+            especialidad: data.especialidad,
+            materia: data.materia,
+            grupo: data.grupo,
+            fecha: fechaEscrita(data.fecha),
+            texto: data.texto
+        });
+
+        try {
+            doc.render();
+            const buf = doc.getZip().generate({ type: 'nodebuffer' });
+
+            // Guardar el DOC en la base de datos
+            const sql = 'INSERT INTO pdf_data (asunto, maestro, especialidad, materia, grupo, fecha, texto, pdf) VALUES (?, ?, ?, ?, ?, ?, ?, ?)';
+            const values = [data.asunto, data.maestro, data.especialidad, data.materia, data.grupo, data.fecha, data.texto, buf];
+
+            connection.query(sql, values, (err, result) => {
+                if (err) {
+                    console.error('Error al guardar el DOC en la base de datos:', err);
+                    return res.status(500).send('Error interno del servidor');
+                }
+
+                console.log('DOC guardado en la base de datos.');
+                res.set({
+                    'Content-Type': 'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+                    'Content-Length': buf.length,
+                    'Content-Disposition': 'attachment; filename="oficio.docx"'
+                });
+                res.send(buf);
+            });
+        } catch (error) {
+            console.error('Error al renderizar el DOC:', error);
+            res.status(500).send('Error interno del servidor');
+        }
+    } else {
+        // Generar el PDF
+        const pdf = new PDFDocument();
+        let buffers = [];
+        pdf.on('data', buffers.push.bind(buffers));
+        pdf.on('end', () => {
+            const pdfData = Buffer.concat(buffers);
+
+            // Guardar el PDF en la base de datos
+            const sql = 'INSERT INTO pdf_data (asunto, maestro, especialidad, materia, grupo, fecha, texto, pdf) VALUES (?, ?, ?, ?, ?, ?, ?, ?)';
+            const values = [data.asunto, data.maestro, data.especialidad, data.materia, data.grupo, data.fecha, data.texto, pdfData];
+
+            connection.query(sql, values, (err, result) => {
+                if (err) {
+                    console.error('Error al guardar el PDF en la base de datos:', err);
+                    return res.status(500).send('Error interno del servidor');
+                }
+
+                console.log('PDF guardado en la base de datos.');
+                res.set({
+                    'Content-Type': 'application/pdf',
+                    'Content-Length': pdfData.length,
+                    'Content-Disposition': 'attachment; filename="oficio.pdf"'
+                });
+                res.send(pdfData);
+            });
+        });
+
+        construirContenidoPDF(pdf);
+    }
 });
-pdf.moveDown(2);
-    pdf.text(` ${data.maestro} \nDOCENTE DEL PLANTEL`);
-    pdf.moveDown(4);
-    pdf.text(`Texto: ${data.texto}`);
-    pdf.moveDown(2);
 
-
-    pdf.moveDown(15);
-    pdf.font('Helvetica-Bold').fontSize(12)
-    .text('ATENTAMENTE.', 70, 480)
-    .text('M.T.I. JOEL VÀZQUEZ RÌOS', 70, 580)
-    .text('JEFE DEL DEPARTAMENTO', 70, 595)    
-    .text('DE SERVICIOS DOCENTES', 70, 610); 
-    
-    agregarImagenEnPieDePagina(pdf);
-
-    pdf.end();
-
-    function fechaEscrita(fecha) {
-      const meses = [
-          'enero', 'febrero', 'marzo', 'abril', 'mayo', 'junio',
-          'julio', 'agosto', 'septiembre', 'octubre', 'noviembre', 'diciembre'
-      ];
-      
-      const partesFecha = fecha.split('-');
-      const dia = partesFecha[2];
-      const mesNum = parseInt(partesFecha[1]) - 1;
-      const mes = meses[mesNum];
-      const año = partesFecha[0];
-      
-      return `${dia} de ${mes} de ${año}`;
-  }
-  
-
-});
 
 // Obtener lista de maestros mediante AJAX
 app.get('/maestro', (req, res) => {
@@ -693,29 +726,34 @@ app.get('/search-pdf', (req, res) => {
 
 // Ruta para descargar un PDF específico
 app.get('/download-pdf/:id', (req, res) => {
-  const pdfId = req.params.id;
+    const pdfId = req.params.id;
 
-  // Consultar el PDF en la base de datos
-  connection.query('SELECT * FROM pdf_data WHERE id = ?', [pdfId], (err, results) => {
-      if (err) {
-          console.error('Error al buscar el PDF en la base de datos:', err);
-          res.status(500).send('Error interno del servidor');
-          return;
-      }
+    // Consultar el PDF en la base de datos
+    connection.query('SELECT * FROM pdf_data WHERE id = ?', [pdfId], (err, results) => {
+        if (err) {
+            console.error('Error al buscar el PDF en la base de datos:', err);
+            return res.status(500).send('Error interno del servidor');
+        }
 
-      // Si se encontró el PDF, enviarlo al cliente
-      if (results.length > 0) {
-          const pdfData = results[0].pdf;
-          res.set({
-              'Content-Type': 'application/pdf',
-              'Content-Disposition': 'attachment; filename="oficio.pdf"'
-          });
-          res.send(pdfData);
-      } else {
-          res.status(404).send('PDF no encontrado');
-      }
-  });
+        // Si se encontró el PDF, enviarlo al cliente
+        if (results.length > 0) {
+            const pdfData = results[0].pdf;
+            const pdfLength = pdfData.length;
+
+            res.set({
+                'Content-Type': 'application/pdf',
+                'Content-Length': pdfLength,
+                'Content-Disposition': 'attachment; filename="oficio.pdf"'
+            });
+            res.end(pdfData); // Envía los datos binarios del PDF al cliente
+        } else {
+            res.status(404).send('PDF no encontrado');
+        }
+    });
 });
+
+app.use(pdfGeneratorRouter);
+
 
 
 app.listen(port, () => {
